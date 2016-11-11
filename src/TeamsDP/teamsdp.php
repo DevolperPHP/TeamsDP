181da1c187b77220c317ee0cc23750429df9eb50
DevolperPHP <devolperphp@gmail.com>
2016-11-11 16:05:20 +0300

    Update teamsdp.php

--- src/TeamsDP/teamsdp.php
+++ src/TeamsDP/teamsdp.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace TeamsDP;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\level\Position;
+use pocketmine\item\Item;
+use pocketmine\inventory\InventoryBase;
+use pocketmine\utils\TextFormat as T;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class teamsdp extends PluginBase implements Listener
